4cf7831eb35a63bd9623f5f6677c9bf0534976b7
Eka Prasetia <ekaone3033@gmail.com>
2020-06-04 13:22:18 +0700

    add recoil stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue
+} from "recoil";
+import CharacterCounter from "./components/CharacterCounter";
 
-function App() {
-  return <>Recoil</>;
+export default function App() {
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
 }
-
-export default App;
